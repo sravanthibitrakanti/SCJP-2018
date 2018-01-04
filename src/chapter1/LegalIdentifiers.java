a5ed9066c02ad2781051eba301a799912c88be37
SAHANA <sravanthi.bitrakanti@github.com>
2018-01-04 15:57:23 -0800

    Initial commit with one java class from scjpchap1

---
+++ src/chapter1/LegalIdentifiers.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class LegalIdentifiers {
+	//Legal identifiers must start with a letter,currency character($),underscroe (_)
+	int a = 10;
+	int b$ = 20;
+	int c_ = 30;
+	//int d@ =40 ; --This gives compile time error 
+	//int d@ is illegal because it has special character.Remember only $ and _ are allowed
+
+}
