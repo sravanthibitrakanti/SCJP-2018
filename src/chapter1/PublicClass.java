f8b1738947dc4a1dbc146f7b79724401d9b00bb4
SAHANA <sravanthi.bitrakanti@github.com>
2018-01-06 10:12:22 -0800

    adding new class bt name PublicClass

---
+++ src/chapter1/PublicClass.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class PublicClass {
+	//class can be either public or default
+	//public class is accessible from any where
+	public void test(){
+		System.out.println("This is just a test method");
+	}
+
+}
