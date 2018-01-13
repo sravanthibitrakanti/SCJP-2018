6236bb49f37d7bffbafafedf167409f482692d9c
SAHANA <sravanthi.bitrakanti@github.com>
2018-01-12 19:13:45 -0800

    adding new test final class

--- src/chapter1/FinalClassDemo1.java
+++ src/chapter1/FinalClassDemo1.java
@@ -1,5 +1,5 @@
 package chapter1;
 
-public class FinalClassDemo1 extends FinalClassDemo {
+//public class FinalClassDemo1 extends FinalClassDemo {
 //This gives compile time error because FinalClassDemo is a final class which can never be subclassed
-}
+//}
---
+++ src/chapter1/Test.java
@@ -0,0 +1,8 @@
+package chapter1;
+//Final classes can only be instantiated.We can never extend a final class
+public final class Test {
+	public void toTest(){
+		System.out.println("to test this method");
+	}
+
+}
